2d87bc208db723cb5309fb4c0f248a0677cbff2e
LuisJann <giannoneluigi10@gmail.com>
2022-10-07 16:18:31 +0200

    Milestone 1 completato

--- js/script.js
+++ js/script.js
@@ -14,3 +14,53 @@ Solo una volta che il milestone 1 sarà completo e funzionante allora realizzere
 l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
 Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina 
 (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). */
+
+// INPUT
+const tripDistanceInput =  document.getElementById("distance");
+const userAgeInput = document.getElementById("age");
+console.log(tripDistanceInput, userAgeInput);
+
+
+const submitBtn = document.getElementById("submit");
+console.log(submitBtn);
+
+// ELABORAZIONE
+submitBtn.addEventListener("click", function() {
+    const tripDistance = parseInt(tripDistanceInput.value);
+    console.log(tripDistance);
+
+    const userAge = parseInt(userAgeInput.value);
+    console.log(userAge);
+
+    const ticketPrice = tripDistance * (0.21);
+    console.log(ticketPrice);
+
+    if (userAge >= 65) {
+        let price = (ticketPrice * 0.6).toFixed(2);
+        console.log(price);
+        document.getElementById("title").innerHTML = `
+        <h1> Ecco il tuo biglietto</h1>
+        <h2> Il prezzo base del tuo biglietto è: ${ticketPrice.toFixed(2)}€</h2>
+        <h2> Grazie allo sconto over 65 pagerai solo: ${price}€</h2>`;
+    }
+    else if (userAge < 18) {
+        let price = (ticketPrice * 0.8).toFixed(2);
+        console.log(price);
+        document.getElementById("title").innerHTML = `
+        <h1> Ecco il tuo biglietto</h1>
+        <h2> Il prezzo base del tuo biglietto è: ${ticketPrice.toFixed(2)}€</h2>
+        <h2> Grazie allo sconto per i minorenni pagerai solo: ${price}€</h2>`;
+    }
+    else {
+        let price = (ticketPrice).toFixed(2);
+        console.log(price);
+        document.getElementById("title").innerHTML = `
+        <h1> Ecco il tuo biglietto</h1>
+        <h2> Il prezzo del tuo biglietto è: ${price}€</h2>`;
+    }
+
+
+    // PULIZIA INPUT
+    tripDistanceInput.value = "";
+    userAgeInput.value = "";
+})
